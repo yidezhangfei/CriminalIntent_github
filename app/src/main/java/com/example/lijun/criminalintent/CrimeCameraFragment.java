7bab95eccb6fb81a1cae998719e9f3a71379252f
李军 <lijunSI0413@sogou-inc.com>
2016-01-28 03:01:03 +0800

    1, 打开并释放相机； 2，确定预览界面大小

--- app/src/main/java/com/example/lijun/criminalintent/CrimeCameraFragment.java
+++ app/src/main/java/com/example/lijun/criminalintent/CrimeCameraFragment.java
@@ -2,12 +2,18 @@ package com.example.lijun.criminalintent;
 
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.hardware.Camera.Size;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by lijun on 16/1/28.
@@ -32,6 +38,46 @@ public class CrimeCameraFragment extends Fragment {
         });
 
         mSurfaceView = (SurfaceView) view.findViewById(R.id.crime_camera_surfaceView);
+        SurfaceHolder surfaceHolder = mSurfaceView.getHolder();
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);  // 干什么用的
+
+        surfaceHolder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                // Tell the camera to use this surface as its preview area
+                try {
+                    if (mCamera != null) {
+                        mCamera.setPreviewDisplay(holder);
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Error setting up preview display", e);
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                if (mCamera == null) return;
+
+                Camera.Parameters parameters = mCamera.getParameters();
+                Size size = getBestSupportedSize(
+                        parameters.getSupportedPictureSizes(), width, height);
+                parameters.setPreviewSize(size.width, size.height);
+                mCamera.setParameters(parameters);
+                try {
+                    mCamera.startPreview();
+                } catch (Exception e) {
+                    Log.e(TAG, "Could not start preview", e);
+                    mCamera.release();
+                    mCamera = null;
+                }
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                if (mCamera != null)
+                    mCamera.stopPreview();
+            }
+        });
 
         return view;
     }
@@ -50,5 +96,20 @@ public class CrimeCameraFragment extends Fragment {
             mCamera.release();
             mCamera = null;
         }
+    }
+
+    public Size getBestSupportedSize(List<Size> sizes, int surfacewidth, int height) {
+        Size bestsize = sizes.get(0);
+        int largestArea = bestsize.width * bestsize.height;
+        for (Size size : sizes) {
+            int area = size.width * size.height;
+            if (size.width > surfacewidth || size.height > height)
+                break;
+            if (area > largestArea) {
+                bestsize = size;
+                largestArea = area;
+            }
+        }
+        return bestsize;
     }
 }
